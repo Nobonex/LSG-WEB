58e113990836f662ad5273ba5ee33961a47900f9
Nobonex <jellecf@live.nl>
2017-12-22 22:54:23 +0100

    Merge branch 'Development' of https://github.com/Nobonex/LSG-WEB into Development
    # Conflicts:
    #	.gitignore

--- src/main/java/com/lsg/website/App.java
+++ src/main/java/com/lsg/website/App.java
@@ -2,8 +2,11 @@ package com.lsg.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("h2")
 public class App
 {
     public static void main(String[] args){
